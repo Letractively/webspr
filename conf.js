fe7883f1963e7c8468ae064ba1bfdf1c8b0f5699
a.d.drummond <a.d.drummond@7ea815a8-e238-0410-a780-91b3ef2cf1cb>
2008-02-16 15:51:29 +0000

    Merge branch 'modularize' into localgooglesvn
    Conflicts:
    
    	LICENSE
    	server.py
    	spr.html

---
+++ conf.js
@@ -0,0 +1,32 @@
+//
+// Set configuration variables.
+//
+
+// Variables common to all experiments.
+var conf_completionMessage = "The results were successfully sent to the server. Thanks!";
+var conf_completionErrorMessage = "There was an error sending the results to the server.";
+var conf_pageTitle = "Experiment";
+var conf_shuffleSequence = seq(equalTo0, rshuffle(lessThan0, greaterThan0));
+var conf_groupChains = [];
+var conf_showProgressBar = true;
+var conf_practiceItems = [];
+var conf_practiceMessage = "Practice";
+
+if (typeof(serverURI) != "undefined")
+    conf_serverURI = serverURI;
+if (typeof(completionMessage) != "undefined")
+    conf_completionMessage = completionMessage;
+if (typeof(completionErrorMessage) != "undefined")
+    conf_completionErrorMessage = completionErrorMessage;
+if (typeof(pageTitle) != "undefined")
+    conf_pageTitle = pageTitle;
+if (typeof(shuffleSequence) != "undefined")
+    conf_shuffleSequence = shuffleSequence;
+if (typeof(groupChains) != "undefined")
+    conf_groupChains = groupChains;
+if (typeof(showProgressBar) != "undefined")
+    conf_showProgressBar = showProgressBar;
+if (typeof(practiceItems) != "undefined")
+    conf_practiceItems = practiceItems;
+if (typeof(practiceMessage) != "undefined")
+    conf_practiceMessage = practiceMessage;
